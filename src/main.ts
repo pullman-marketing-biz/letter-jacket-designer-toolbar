839b3c34138a148e80b045d175f9e67390934b3e
Lucas Reis <lucas@mreis.dev>
2023-07-27 15:54:28 -0300

    setup dependencies to vue js

--- src/main.ts
+++ src/main.ts
@@ -1,11 +1,14 @@
-import './assets/main.css'
-
 import { createApp } from 'vue'
 import { createPinia } from 'pinia'
+import PrimeVue from 'primevue/config'
 import App from './App.vue'
+
+import 'primevue/resources/themes/lara-light-indigo/theme.css'
+import 'primeicons/primeicons.css'
 
 const app = createApp(App)
 
 app.use(createPinia())
+app.use(PrimeVue)
 
 app.mount('#app')
