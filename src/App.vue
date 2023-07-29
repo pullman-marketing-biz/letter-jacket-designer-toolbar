d80a06cac5504f6de7ff27f76cd1068abba02b4b
Lucas Reis <lucas@mreis.dev>
2023-07-29 01:35:03 -0300

    provide function to display the loading spinner

--- src/App.vue
+++ src/App.vue
@@ -1,6 +1,20 @@
 <template lang="pug">
-Toolbar
+div
+    Toolbar
+    LoadingSpinner(v-if="loadingSpinnerVisible")
 </template>
 <script setup lang="ts">
+import { ref, provide } from 'vue'
 import Toolbar from './components/Toolbar.vue'
+import LoadingSpinner from './components/LoadingSpinner.vue'
+
+const loadingSpinnerVisible = ref(false)
+
+function showLoadingSpinner(value: boolean) {
+    loadingSpinnerVisible.value = value
+}
+
+provide('loadingSpinner', {
+    showLoadingSpinner
+})
 </script>
